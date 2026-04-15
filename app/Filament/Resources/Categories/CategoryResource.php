7af6f5ab4a7f4e08a77e3accafbddbe3b1a34156
syaifulestrada <syaifultheminister@gmail.com>
2026-04-15 18:50:38 +0700

    Change category iconn resource into tag

--- app/Filament/Resources/Categories/CategoryResource.php
+++ app/Filament/Resources/Categories/CategoryResource.php
@@ -21,7 +21,7 @@ class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Tag;
 
     protected static ?string $recordTitleAttribute = 'name';
 
