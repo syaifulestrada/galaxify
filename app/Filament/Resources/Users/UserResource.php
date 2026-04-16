c30235f913e2a17f574d6d7f9848c595161f951e
syaifulestrada <syaifultheminister@gmail.com>
2026-04-16 09:47:05 +0700

    Assign user management group into user module

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Users;
 
+use App\Enum\NavigationGroup;
 use App\Filament\Resources\Users\Pages\CreateUser;
 use App\Filament\Resources\Users\Pages\EditUser;
 use App\Filament\Resources\Users\Pages\ListUsers;
@@ -15,12 +16,15 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::User;
+
+    protected static string|UnitEnum|null $navigationGroup = NavigationGroup::UserManagement;
 
     protected static ?string $recordTitleAttribute = 'name';
 
