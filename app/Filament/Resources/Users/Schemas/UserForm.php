6d0c3270ee188496ff885fc960deb1db72f613c5
syaifulestrada <syaifultheminister@gmail.com>
2026-04-16 09:22:38 +0700

    Modify user form and change avatar input into fileupload

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+
+use function Livewire\on;
 
 class UserForm
 {
@@ -12,17 +16,21 @@ class UserForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                DateTimePicker::make('email_verified_at'),
-                TextInput::make('password')
-                    ->password()
-                    ->required(),
-                TextInput::make('avatar'),
+                Section::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+                        TextInput::make('email')
+                            ->label('Email address')
+                            ->email()
+                            ->required(),
+                        TextInput::make('password')
+                            ->password()
+                            ->visibleOn('create')
+                            ->required(),
+                        FileUpload::make('avatar'),
+                    ])
+                    ->columnSpanFull()
             ]);
     }
 }
