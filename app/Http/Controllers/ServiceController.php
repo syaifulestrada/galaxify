66c69f2e6868c1d4eac2d90a0c6593d1ad5430b0
syaifulestrada <syaifultheminister@gmail.com>
2026-04-30 12:34:52 +0700

    Display the services data from controller

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Services;
+
 class ServiceController extends Controller
 {
     public function index()
     {
+        $services = Services::get();
+
         return view('services.index', [
+            'services' => $services,
             'subtitle' => 'Services',
         ]);
     }
