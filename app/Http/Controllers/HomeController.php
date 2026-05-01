744f3ce42e6028f7d58d84366ebbf3c52df7a12c
syaifulestrada <syaifultheminister@gmail.com>
2026-05-01 10:42:50 +0700

    Load services data from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\AboutUs;
+use App\Models\Services;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $aboutUs = AboutUs::first();
+        $services = Services::get()->take(4);
 
         return view('welcome', [
             'aboutUs' => $aboutUs,
+            'services' => $services,
         ]);
     }
 }
