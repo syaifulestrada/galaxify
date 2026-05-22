1c56094774967ed04920fe6e4ebc142d31d5dedf
syaifulestrada <syaifultheminister@gmail.com>
2026-05-22 10:06:18 +0700

    Create morph to many relation to categories and techStack in Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Support\Facades\Storage;
 
-#[Fillable(['title', 'slug', 'year', 'client', 'duration', , 'content', 'cover', 'active'])]
+#[Fillable(['title', 'slug', 'year', 'client', 'duration', 'content', 'cover', 'active'])]
 class Project extends Model
 {
     protected static function booted(): void
@@ -16,5 +17,17 @@ class Project extends Model
                 Storage::disk('public')->delete($project->cover);
             }
         });
+    }
+
+    public function categories(): MorphToMany
+    {
+        return $this->morphToMany(Category::class, 'categorizable')
+            ->where('type', 'project');
+    }
+
+    public function techStacks(): MorphToMany
+    {
+        return $this->morphToMany(Category::class, 'categorizable')
+            ->where('type', 'tech_stack');
     }
 }
