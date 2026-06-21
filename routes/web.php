84a2c70555f5f938d6b9929dc0dc3adbb0d80a67
syaifulestrada <syaifultheminister@gmail.com>
2026-06-21 15:45:16 +0700

    Create MemberController resource

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\AboutUs\AboutUsController;
 use App\Http\Controllers\Home\HomeController;
-use App\Http\Controllers\MemberController;
+use App\Http\Controllers\Member\MemberController;
 use App\Http\Controllers\Project\ProjectController;
 use App\Http\Controllers\Service\ServiceController;
 use App\Livewire\Posts\Postsindex;
@@ -15,12 +15,8 @@ Route::get('/aboutus', AboutUsController::class)->name('aboutus');
 Route::resources([
     'projects' => ProjectController::class,
     'services' => ServiceController::class,
+    'members' => MemberController::class,
 ]);
-
-Route::controller(MemberController::class)->group(function () {
-    Route::get('/members', 'index')->name('members');
-    Route::get('/members/{member}', 'show')->name('members.show');
-});
 
 Route::get('/posts', Postsindex::class)->name('posts.index');
 Route::get('/posts/{slug}', ShowPost::class)->name('posts.show');
